74db3c57ea95858485da9c2ad8456c6327f2b8a4
Paolo_X86 <p.ferri1986@gmail.com>
2020-03-28 21:36:51 +0800

    GUI instructions

---
+++ VirtualTools/Assets/Scripts/GUI/MainCanvas.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainCanvas : MonoBehaviour
+{
+    private GameObject m_dog;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_dog = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DogSpeak(string text)
+    {
+        SetDogEnabled(true);
+        m_dog.transform.GetComponentInChildren<Text>().text = text;
+    }
+
+    public void SetDogEnabled(bool enabled)
+    {
+        m_dog.SetActive(enabled);
+    }
+
+    private IEnumerator PopUpMessage(float seconds, string text)
+    {
+        DogSpeak(text);
+        yield return new WaitForSeconds(seconds);
+        SetDogEnabled(false);
+    }
+    public void DogPopUp(float seconds, string text)
+    {
+        StartCoroutine(PopUpMessage(seconds, text));
+    }
+
+    private IEnumerator InstructionSequence(string[] instructions)
+    {
+        int index = 0;
+        while(index < instructions.Length)
+        {
+            DogSpeak(instructions[index]);
+            if (Input.GetButton("Fire1"))
+            {
+                index++;
+                if (index >= instructions.Length) break;
+
+                DogSpeak(instructions[index]);
+                yield return new WaitForSeconds(0.5f);
+            }
+            else
+                yield return null;
+        }
+        SetDogEnabled(false);
+    }
+
+    public void DogInstructionSequence(string[] instructions)
+    {
+        StartCoroutine(InstructionSequence(instructions));
+    }
+}
---
+++ VirtualTools/Assets/Scripts/Managers/GUIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour
+{
+    private static GUIManager m_instance;
+    private MainCanvas m_mainCanvas;
+    public static GUIManager Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                GameObject go = new GameObject();
+                m_instance = go.AddComponent<GUIManager>();
+            }
+            return m_instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_mainCanvas = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<MainCanvas>();
+
+        string[] instructions = { "Hi, I'm Gustaf Von Horbert. Press the Fire button to dismiss my messages.", "How are you?", "Fuck off then!" };
+        m_mainCanvas.DogInstructionSequence(instructions);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
