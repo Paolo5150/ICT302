1446bb505bbf998e5e9a1b32c57ed6a89a6fc93d
Paolo_X86 <p.ferri1986@gmail.com>
2020-04-03 21:25:37 +0800

    Basic login

---
+++ VirtualTools/Assets/Scripts/GUI/LoginUI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginUI : MonoBehaviour
+{
+    InputField m_idField;
+    InputField m_pswField;
+    Button m_loginButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_idField = GameObject.Find("IdField").GetComponent<InputField>();
+        m_pswField = GameObject.Find("PswField").GetComponent<InputField>();
+        m_loginButton = GameObject.Find("LogInButton").GetComponent<Button>();
+        m_loginButton.onClick.AddListener(Click);
+
+    }
+
+    private void Click()
+    {
+        string id = m_idField.text;
+        string psw = m_pswField.text;
+
+        // Validate data
+
+        WWWForm form = new WWWForm();
+        form.AddField("id", id);
+        form.AddField("password", psw);
+
+        NetworkManager.Instance.SendRequest(form, "login.php", (string reply) => {
+            
+            if(reply == "ok")
+            {
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                Text dogText = GameObject.Find("DogText").GetComponent<Text>();
+                dogText.text = "ID or password incorrect.";
+            }
+        },
+        
+        () => {
+
+        });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VirtualTools/Assets/Scripts/Managers/NetworkManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManager : MonoBehaviour
+{
+
+    public static string SERVER_ADDRESS = "https://unreckoned-worry.000webhostapp.com/";
+
+    private static NetworkManager m_instance;
+
+    public static NetworkManager Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                GameObject coreGameObject = GameObject.Find("Managers");
+                m_instance = coreGameObject.AddComponent<NetworkManager>();
+            }
+
+            return m_instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (m_instance == null)
+            m_instance = GetComponent<NetworkManager>();
+        else
+            DestroyImmediate(this);
+    }
+
+    public void SendRequest(WWWForm form, string targetScript, Action<String> onSuccess, Action onFail)
+    {
+        StartCoroutine(SendPostRequest(form, targetScript, onSuccess, onFail));
+    }
+
+    private IEnumerator SendPostRequest(WWWForm form, string targetScript, Action<String> onSuccess, Action onFail)
+    {
+        Debug.Log("Sending to: " + SERVER_ADDRESS + targetScript);
+        UnityWebRequest www = UnityWebRequest.Post(SERVER_ADDRESS + targetScript, form);
+        yield return www.SendWebRequest();
+        
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            onFail();
+            Debug.Log(www.error);
+        }
+        else
+        {
+            onSuccess(www.downloadHandler.text);
+        }
+    }
+}
