c61331258974c3aeea03c7c8325b34d6179e5c8c
Paolo_X86 <p.ferri1986@gmail.com>
2020-04-14 18:28:22 +0800

    kinda merged

--- VirtualTools/Assets/Scripts/GUI/MainCanvas.cs
+++ VirtualTools/Assets/Scripts/GUI/MainCanvas.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 public class MainCanvas : MonoBehaviour
 {
     private GameObject m_dog;
+    private GameObject m_results;
 
     public void Init()
     {
         m_dog = transform.GetChild(0).gameObject;
+        m_results = transform.GetChild(1).gameObject;
 
     }
     // Start is called before the first frame update
@@ -72,5 +74,22 @@ public class MainCanvas : MonoBehaviour
     public void DogInstructionSequence(string[] instructions, Action action)
     {
         StartCoroutine(InstructionSequence(instructions, action));
+    }
+
+    public void DisplayResults(bool success, string name, string studentNumber, string date, string startTime, string endTime, int retries)
+    {
+        m_results.SetActive(true);
+
+        if (success)
+            m_results.transform.GetChild(0).GetComponent<Text>().text = "Success!";
+        else
+            m_results.transform.GetChild(0).GetComponent<Text>().text = "Failed";
+
+        m_results.transform.GetChild(1).GetComponent<Text>().text = "Name: " + name;
+        m_results.transform.GetChild(2).GetComponent<Text>().text = "Student Number: " + studentNumber;
+        m_results.transform.GetChild(3).GetComponent<Text>().text = "Date: " + date;
+        m_results.transform.GetChild(4).GetComponent<Text>().text = "Start Time: " + startTime;
+        m_results.transform.GetChild(5).GetComponent<Text>().text = "End Time: " + endTime;
+        m_results.transform.GetChild(6).GetComponent<Text>().text = "Retries: " + retries.ToString();
     }
 }
