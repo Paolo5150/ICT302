a436fa0b998c5e4f7cc9a3ce68f3ef0c750b821e
elizeon <l.haynes070@gmail.com>
2020-04-25 19:11:36 +0800

    Begin implementing instrument location slots

---
+++ VirtualTools/Assets/Scripts/Managers/InstrumentLocManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstrumentLocManager : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> InstrumentLocationSlots;
+    [SerializeField]
+    List<GameObject> InstrumentPrefabs;
+    void InstantiateInstrumentsInOrder(List<Instrument> instruments)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
